91c3ab5562f5dc605e006a2591cbb2cc50fede71
Bartticus <kbartter@gmail.com>
2025-12-27 21:58:54 -0800

    Fixed bug, game runs now

--- Scenes/Player/player_default.gd
+++ Scenes/Player/player_default.gd
@@ -1,8 +1,14 @@
 class_name PlayerDefault
 extends State
 
+@export var dragon_mesh: MeshInstance3D
+@export var goblin_mesh: MeshInstance3D
+@export var minotaur_mesh: MeshInstance3D
+@export var wolf_mesh: MeshInstance3D
+
+
 func enter():
-	await get_tree().create_timer(0.1).timeout
+	await get_tree().create_timer(0.1).timeout #throws an error without this
 	await Global.player.mask_changed
 	change_state()
 
@@ -10,31 +16,30 @@ func exit():
 	pass
 
 func process(_delta: float) -> void:
-	if Global.player.current_mask == "Dragon":
-		transitioned.emit(self, "PlayerDragon")
-	
+	pass
+
 func physics_process(_delta: float) -> void:
 	pass
 
 func change_state() -> void:
 	match Global.player.current_mask:
 		"Dragon":
-			$DragonMesh.show()
+			dragon_mesh.show()
 			transitioned.emit(self, "PlayerDragon")
 			#other dragon stuff
 			pass
 		"Goblin":
-			$GoblinMesh.show()
+			goblin_mesh.show()
 			transitioned.emit(self, "PlayerGoblin")
 			#goblin stuff
 			pass
 		"Minotaur":
-			$MinotaurMesh.show()
+			minotaur_mesh.show()
 			transitioned.emit(self, "PlayerMinotaur")
 			#mino stuff
 			pass
 		"Wolf":
-			$WolfMesh.show()
+			wolf_mesh.show()
 			transitioned.emit(self, "PlayerWolf")
 			#wolf stuff
 			pass
